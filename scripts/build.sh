94a397a8c7763bbf959889e39ff29d8ae032caea
renq <renq@olymtech.com>
2020-02-02 19:11:08 +0800

    eureka 指定使用哪一个网卡地址

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 
 # apollo config db info
-apollo_config_db_url=jdbc:mysql://fill-in-the-correct-server:3306/ApolloConfigDB?characterEncoding=utf8
-apollo_config_db_username=FillInCorrectUser
-apollo_config_db_password=FillInCorrectPassword
+spring.datasource.url = jdbc:mysql://rm-bp149t3or0eb470956o.mysql.rds.aliyuncs.com:3306/apolloconfigdb?useUnicode=true&characterEncoding=UTF-8&allowMultiQueries=true&zeroDateTimeBehavior=convertToNull
+spring.datasource.username = saas1
+spring.datasource.password = olymtech1
 
 # apollo portal db info
-apollo_portal_db_url=jdbc:mysql://fill-in-the-correct-server:3306/ApolloPortalDB?characterEncoding=utf8
-apollo_portal_db_username=FillInCorrectUser
-apollo_portal_db_password=FillInCorrectPassword
+spring.datasource.url = jdbc:mysql://rm-bp149t3or0eb470956o.mysql.rds.aliyuncs.com:3306/apolloportaldb?useUnicode=true&characterEncoding=UTF-8&allowMultiQueries=true&zeroDateTimeBehavior=convertToNull
+spring.datasource.username = saas1
+spring.datasource.password = olymtech1
 
 # meta server url, different environments should have different meta server addresses
-dev_meta=http://fill-in-dev-meta-server:8080
-fat_meta=http://fill-in-fat-meta-server:8080
-uat_meta=http://fill-in-uat-meta-server:8080
-pro_meta=http://fill-in-pro-meta-server:8080
+dev_meta=http://120.27.151.176:8081
 
 META_SERVERS_OPTS="-Ddev_meta=$dev_meta -Dfat_meta=$fat_meta -Duat_meta=$uat_meta -Dpro_meta=$pro_meta"
 
